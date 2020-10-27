18f7488e4a99dd24ce91af4ca17a5dfa81f09317
Samara Islam <sz.islam@outlook.com>
2020-10-27 16:21:17 -0700

    conditionals

---
+++ Exercises/conditionals.rb
@@ -0,0 +1,81 @@
+# 1.
+# 
+
+sun = ['visible', 'hidden'].sample
+
+#if sun == 'visible'
+#  puts "The sun is so bright!"
+#else
+#  puts "The sun is hidden."
+#end
+
+# 2.
+#
+
+#unless sun == 'visible'
+#  puts "The clouds are blocking the sun."
+#end
+
+# 3
+#
+
+#if sun == 'visible'
+#  puts 'The sun is so bright!'
+#end
+
+#unless sun == 'visible'
+#  puts 'The clouds are blocking the sun'
+#end
+
+# 4.
+#
+
+#boolean = [true, false].sample
+#
+#choice = boolean ? "I'm True" : "I'm false"
+#
+#p choice
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+#case stoplight
+#when 'green'
+#  puts 'go'
+#when 'yellow'
+#  puts 'slow down'
+#else 
+#  puts 'stop'
+#end
+
+#if stoplight == 'green'
+#  puts 'go'
+#elsif stoplight == 'yellow'
+#  puts 'slow down'
+#else 
+#  puts 'stop'
+#end
+
+#status = ['awake', 'tired'].sample
+#
+#alert = if status == 'awake'
+#          "Be productive"
+#        else
+#          "Go to sleep"
+#        end
+#
+#puts alert
+
+#number = rand(10)
+#
+#if number == 5
+#  puts '5 is cool'
+#else
+#  puts 'another numbers are cool'
+#end
+
+#case stoplight
+#when 'green' then puts 'go'
+#when 'yellow' then puts 'slow down'
+#else puts 'stop'
+#end
+
