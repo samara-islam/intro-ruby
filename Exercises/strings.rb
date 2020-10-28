819cadf320d8efd713ecd88899d615b3e4c6ab8b
Samara Islam <sz.islam@outlook.com>
2020-10-28 10:48:16 -0700

    exercises: strings

---
+++ Exercises/strings.rb
@@ -0,0 +1,50 @@
+
+# empty_string = String.new == emptystring = ''
+
+# puts "It's now 12 o' clock"
+
+# casecmp compares the value of two strings while ignoring the case
+# when both string equal, casecmp returns 0
+
+#name = 'Roger'
+#puts name.casecmp('RoGeR') == 0
+#puts name.casecmp('Dave') == 0
+
+#name = 'Lizzy'
+#puts "Hello, #{name}"
+
+#first_name = 'John'
+#last_name = 'Doe'
+#
+#full_name = "#{first_name} #{last_name}"
+#
+#p full_name
+
+#state = 'tExAs'.capitalize
+#puts state
+
+#greeting = 'Hello!'
+#greeting.gsub!('Hello!','Goodbye!')
+#puts greeting 
+
+#alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#puts alphabet.split('')
+
+#words = 'car human elephant airplane'
+#arr = words.split(' ')
+#arr.each { |element| puts "#{element}s"}
+
+#colors = 'blue pink yellow orange'
+#arr_colors = colors.split
+#
+#if arr_colors.include?('yellow')
+#  puts 'true'
+#else
+#  puts 'false'
+#end
+#
+#if arr_colors.include?('purple')
+#  puts 'true'
+#else
+#  puts 'false'
+#end
