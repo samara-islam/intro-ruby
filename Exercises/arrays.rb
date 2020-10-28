402cd6c8ea08fa72b010eaf3a800a56be7f5550b
Samara Islam <sz.islam@outlook.com>
2020-10-28 13:34:45 -0700

    stuff

---
+++ Exercises/arrays.rb
@@ -0,0 +1,18 @@
+#pets = ['cat','dog','fish','lizard']
+#my_pet = pets[2, 3]
+
+#puts "I have #{my_pet[0]} and #{my_pet[1]}"
+#my_pet.pop
+
+#puts "my pet is #{my_pet[0]}"
+
+#my_pet.push(pets[1])
+#puts "I have pet #{my_pet[0]} and pet #{my_pet[1]}"
+
+#colors = ['red', 'yellow', 'purple', 'green']
+#colors.each { |element| puts "I'm the color #{element}!" }
+
+#numbers = [1, 2, 3, 4, 5]
+#doubled_numbers = []
+#doubled_numbers = numbers.map { |element| element * 2 }
+#puts doubled_numbers
